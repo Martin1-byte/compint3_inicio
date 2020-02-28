76d4f877b941dc7dfd5e0266808aea31ddd47b05
Martin1-byte <60704373+Martin1-byte@users.noreply.github.com>
2020-02-28 10:55:17 -0600

    examen1
    Martin Beltran Aviles

---
+++ examen1/scripts.js
@@ -0,0 +1,15 @@
+function mostrarFoto6(){
+    document.getElementById("foto").style.display = "block";
+
+}
+    
+
+function ocultarFoto6(){
+    document.getElementById("foto").style.display = "none";
+    
+}
+
+function validarTelefono6(){
+  
+
+}
